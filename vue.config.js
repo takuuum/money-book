25601efa72f0170a005fab5bb6bfa2cfbf4feb43
水野拓海 <taku.m.poo.0705@gmail.com>
2021-03-14 22:20:34 +0900

    production用の設定を追加

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
-}
+  ],
+  publicPath: isProd ? '/money-book' : '/',
+  outputDir: 'docs',
+  filenameHashing: false,
+  productionSourceMap: false
