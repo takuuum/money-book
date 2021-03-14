9d1f6163fdda773a455a240a2f8bafca50b5e564
水野拓海 <taku.m.poo.0705@gmail.com>
2021-03-14 22:13:35 +0900

    家計簿

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,56 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <!-- ツールバー -->
+    <v-app-bar app color="green" dark>
+      <!-- タイトル -->
+      <v-toolbar-title>{{ appName }}</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <!-- テーブルアイコンのボタン -->
+      <v-btn icon to="/">
+        <v-icon>mdi-file-table-outline</v-icon>
+      </v-btn>
+      <!-- 歯車アイコンのボタン -->
+      <v-btn icon to="/settings">
+        <v-icon>mdi-cog</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <!-- メインコンテンツ -->
+    <v-main>
+      <v-container fluid>
+        <!-- router-view の中身がパスによって切り替わる -->
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+    <!-- スナックバー -->
+    <v-snackbar v-model="snackbar" color="error">{{ errorMessage }}</v-snackbar>
+  </v-app>
 </template>
+<script>
+import { mapState } from 'vuex'
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+export default {
+  name: 'App',
 
-#nav {
-  padding: 30px;
-}
+  data () {
+    return {
+      snackbar: false
+    }
+  },
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  computed: mapState({
+    appName: state => state.settings.appName,
+    errorMessage: state => state.errorMessage
+  }),
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+  watch: {
+    errorMessage () {
+      this.snackbar = true
+    }
+  },
+
+  // Appインスタンス生成前に一度だけ実行されます
+  beforeCreate () {
+    this.$store.dispatch('loadSettings')
+  }
 }
-</style>
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,29 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Settings from '../views/Settings.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/',       // パスが"/"のときの設定
+    name: 'Home',    // このルートに”Home”という名前をつける
+    component: Home  // router-viewの中にHomeコンポーネントを表示する
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/settings',
+    name: 'Settings',
+    component: Settings
   }
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
